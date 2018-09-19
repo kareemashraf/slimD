7969db2c7393f9133571a879d72d8154577a7099
kareemessawy <k.essawy@cliqdigital.com>
2018-09-19 14:40:25 +0200

    use aws_php_sdk

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -12,6 +12,7 @@ use SimpleEmailServiceMessage;
 use SimpleEmailService;
 use Aws\CloudWatch\CloudWatchClient;
 use Aws\Exception\AwsException;
+use Aws\Ses\SesClient;
 
 class AjaxController extends Controller
 {
@@ -51,42 +52,42 @@ class AjaxController extends Controller
      */
     public function opens(Request $request)
     {
-        $method = $request->request->get("method");
-        $year = '2018'; //  $request->request->get("year");
-        $usr= $this->get('security.token_storage')->getToken()->getUser();
-        $em = $this->getDoctrine()->getManager();
-
-        if ($method == "opened") {
-
-            $RAW_QUERY = "select   count(*) as total, DATE_FORMAT(`opened_date`, '%b') as month from `track` 
-                      WHERE DATE_FORMAT(`opened_date`, '%Y') = '" . $year . "' 
-                      AND user_id = '" . $usr->getId() . "' AND opened = 1 
-                      GROUP BY DATE_FORMAT(`opened_date`, '%Y-%m') 
-                      ORDER BY opened_date asc;";
-
-            $statement = $em->getConnection()->prepare($RAW_QUERY);
-            $statement->execute();
-
-            $result = $statement->fetchAll();
-
-            return $this->json($result);
-        }
-        elseif ($method == "sent"){
-            $RAW_QUERY = "select   count(*) as total, DATE_FORMAT(`opened_date`, '%b') as month from `track`  
-                          WHERE DATE_FORMAT(`sent_date`, '%Y') = '" . $year . "' 
-                          AND user_id = '". $usr->getId() ."'
-                          GROUP BY DATE_FORMAT(`sent_date`, '%Y-%m') 
-                          ORDER BY sent_date asc;";
-
-            $statement = $em->getConnection()->prepare($RAW_QUERY);
-            $statement->execute();
-
-            $result = $statement->fetchAll();
-
-            return $this->json($result);
-        }
-
-        return false;
+//        $method = $request->request->get("method");
+//        $year = '2018'; //  $request->request->get("year");
+//        $usr= $this->get('security.token_storage')->getToken()->getUser();
+//        $em = $this->getDoctrine()->getManager();
+//
+//        if ($method == "opened") {
+//
+//            $RAW_QUERY = "select   count(*) as total, DATE_FORMAT(`opened_date`, '%b') as month from `track`
+//                      WHERE DATE_FORMAT(`opened_date`, '%Y') = '" . $year . "'
+//                      AND user_id = '" . $usr->getId() . "' AND opened = 1
+//                      GROUP BY DATE_FORMAT(`opened_date`, '%Y-%m')
+//                      ORDER BY opened_date asc;";
+//
+//            $statement = $em->getConnection()->prepare($RAW_QUERY);
+//            $statement->execute();
+//
+//            $result = $statement->fetchAll();
+//
+//            return $this->json($result);
+//        }
+//        elseif ($method == "sent"){
+//            $RAW_QUERY = "select   count(*) as total, DATE_FORMAT(`opened_date`, '%b') as month from `track`
+//                          WHERE DATE_FORMAT(`sent_date`, '%Y') = '" . $year . "'
+//                          AND user_id = '". $usr->getId() ."'
+//                          GROUP BY DATE_FORMAT(`sent_date`, '%Y-%m')
+//                          ORDER BY sent_date asc;";
+//
+//            $statement = $em->getConnection()->prepare($RAW_QUERY);
+//            $statement->execute();
+//
+//            $result = $statement->fetchAll();
+//
+//            return $this->json($result);
+//        }
+//
+//        return false;
     }
 
 
@@ -100,23 +101,89 @@ class AjaxController extends Controller
     {
 
 
-        $trigger_error = true;
-        $region_endpoint = SimpleEmailService::AWS_EU_WEST1;
-        $ses = new SimpleEmailService('', '', $region_endpoint,$trigger_error);
+//        $trigger_error = true;
+//        $region_endpoint = SimpleEmailService::AWS_EU_WEST1;
+//        $ses = new SimpleEmailService('', '', $region_endpoint,$trigger_error);
+//
+//        $m = new SimpleEmailServiceMessage();
+//        $m->setConfigurationSet('tracking');
+//        $m->addTo('kareem.ashraf.91@gmail.com');
+//        $m->setFrom('kareem.ashraf.91@gmail.com');
+//        $m->setSubject('hi');
+//        $m->setMessageFromString('This is the message body.','<h1>This is the message body. </h1><a href="http://mailgram.online" >mailgram</a> ');
+//
+//
+//        $result = $ses->sendEmail($m, false, $trigger_error);
+//
+//        var_dump($result); die();
 
-        $m = new SimpleEmailServiceMessage();
-        $m->setConfigurationSet('tracking');
-        $m->addTo('kareem.ashraf.91@gmail.com');
-        $m->setFrom('kareem.ashraf.91@gmail.com');
-        $m->setSubject('hi');
-        $m->setMessageFromString('This is the message body.','<h1>This is the message body. </h1><a href="http://mailgram.online" >mailgram</a> ');
+
+        $key = ''; //key
+        $secret = ''; //secret
+
+        $SesClient = new SesClient([
+            'region' => 'eu-west-1',
+            'version' => '2010-12-01', //'latest'
+            'credentials' => [
+                'key' => $key,
+                'secret' => $secret,
+            ],
+        ]);
 
 
-        $result = $ses->sendEmail($m, false, $trigger_error);
+        $sender_email = '"test name" <kareem.ashraf.91@gmail.com>';
 
-        var_dump($result); die();
+        $recipient_emails = ['kareem.ashraf.91@gmail.com'];
+
+        $configuration_set = 'tracking';
+
+        $subject = 'Amazon SES test (AWS SDK for PHP)';
+        $plaintext_body = 'This email was sent with Amazon SES using the AWS SDK for PHP.' ;
+        $html_body =  '<h1>AWS Amazon Simple Email Service Test Email</h1>'.
+            '<p>This email was sent with <a href="https://aws.amazon.com/ses/">'.
+            'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">'.
+            'AWS SDK for PHP</a>.</p>';
+        $char_set = 'UTF-8';
+
+        try {
+            $result = $SesClient->sendEmail([
+                'Destination' => [
+                    'ToAddresses' => $recipient_emails,
+                ],
+                'ReplyToAddresses' => [$sender_email],
+                'Source' => $sender_email,
+                'Message' => [
+                    'Body' => [
+                        'Html' => [
+                            'Charset' => $char_set,
+                            'Data' => $html_body,
+                        ],
+                        'Text' => [
+                            'Charset' => $char_set,
+                            'Data' => $plaintext_body,
+                        ],
+                    ],
+                    'Subject' => [
+                        'Charset' => $char_set,
+                        'Data' => $subject,
+                    ],
+                ],
+                // If you aren't using a configuration set, comment or delete the
+                // following line
+                'ConfigurationSetName' => $configuration_set,
+            ]);
+            var_dump($result);
+            $messageId = $result['MessageId'];
+            echo("Email sent! Message ID: $messageId"."\n");
+        } catch (AwsException $e) {
+            // output error message if fails
+            echo $e->getMessage();
+            echo("The email was not sent. Error message: ".$e->getAwsErrorMessage()."\n");
+            echo "\n";
+        }
 
 
+        die;
     }
 
     /**
@@ -128,7 +195,7 @@ class AjaxController extends Controller
 
         $client = new CloudWatchClient([
             'region' => 'eu-west-1',
-            'version' => '2010-08-01',
+            'version' => '2010-08-01', // 'latest' ?
             'credentials' => [
                 'key' => $key,
                 'secret' => $secret,
